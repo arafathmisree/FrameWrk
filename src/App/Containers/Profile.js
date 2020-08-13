63ca9dfb0b6af8144c1a2a1bab45c573e6bd66f4
parthipan-swivel <parthipan@swivelgroup.com.au>
2020-08-13 13:58:22 +0530

    User profile image upload

--- src/App/Containers/Profile.js
+++ src/App/Containers/Profile.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../assets/styles/main.css";
 
 import { Button } from "../Components/atoms/Button";
 import { Card } from "../Components/atoms/Card";
 import { Typography } from "../Components/atoms/Typography";
+import ImageUploader from "react-images-upload";
 
 import { Textfield } from "../Components/atoms/Textfield";
 import { ImageComponent } from "../Components/atoms/ImageComponent";
 
 function Profile() {
+  //Set Uploaded Picture
+  function onDrop(file) {
+    setPicture(URL.createObjectURL(file[0]));
+  }
+
+  //Default Picture
+  const [picture, setPicture] = useState(
+    "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
+  );
   return (
     <div>
       <div className="flex justify-center p-8 md:pt-24">
@@ -25,13 +35,20 @@ function Profile() {
               borderWidth: 10,
             }}
           >
-            <ImageComponent
-              image={
-                "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
-              }
+            <ImageComponent image={picture} />
+          </div>
+          <div>
+            <ImageUploader
+              withIcon={true}
+              onChange={onDrop}
+              imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+              maxFileSize={5242880}
+              buttonText={"Change Picture"}
+              label={""}
+              singleImage={true}
+              withIcon={false}
             />
           </div>
-
           <div className="mt-4">
             <Textfield label="First name"></Textfield>
             <Textfield label="Last name"></Textfield>
