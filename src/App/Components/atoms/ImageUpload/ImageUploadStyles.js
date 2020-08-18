22e53d281934706b56904a6d3388f67ff35f8818
parthipan-swivel <parthipan@swivelgroup.com.au>
2020-08-18 12:20:58 +0530

    image upload library removed

--- src/App/Components/atoms/ImageUpload/ImageUploadStyles.js
+++
@@ -1,8 +0,0 @@
-export const ImageUploadType = {
-  primary: "border border-gray-400 flex flex-col p-4 rounded shadow",
-};
-
-export const ImageUploadSize = {
-  small: "w-full md:w-1/4",
-  half: "w-full md:w-1/2",
-};
