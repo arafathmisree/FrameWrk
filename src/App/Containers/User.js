b9fbd6f29b535e1226b8c187f88ee190be69ddc4
arafathmisree <arafath.misree@tokoin.io>
2020-08-19 17:47:02 +0530

    Changes to merge

--- src/App/Containers/User.js
+++ src/App/Containers/User.js
@@ -8,16 +8,11 @@ import StartupActions from "../Stores/Startup/Actions";
 
 function User(props) {
 
-  const logOut = () => {
-    props.logOut()
-  };
+
   return (
     <div>
       <h2>User area</h2>
 
-      <Button onClick={logOut} type="outline" className="ml-2">
-        Log Out
-      </Button>
     </div>
   );
 }
@@ -25,7 +20,7 @@ function User(props) {
 const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = (dispatch) => ({
-  logOut: (user) => dispatch(StartupActions.logOut()),
+ 
 
 });
 export default connect(mapStateToProps, mapDispatchToProps)(User);
--- src/App/Navigators/NavBar.js
+++ src/App/Navigators/NavBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import AdminBar from "../Navigators/NavBar/Admin"
 import UserBar from "../Navigators/NavBar/User"
 
